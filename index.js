f3d3effab49d840ea489e6e6ec7885dced7858b3
Klemense <klemenseloh@gmail.com>
2022-02-03 14:33:58 -0500

    commit tree 92a85d47988a46d3c7381bfe881a2ff0a5c84ba0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+/*
+const findMatching = (a,b) => a.filter(a => a.toLowerCase() === b.toLowerCase())
+
+const fuzzyMatch = (a,b) => a.filter(a => a.charAt(0) === b.charAt(0))
+
+const matchName = (a,b) => a.filter(a => a.name === b)
+
+*/
+
+function findMatching(drivers, str){
+    return drivers.filter(names => names.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(names=> names.slice(0, str.length).toLowerCase() === str.toLowerCase());
+}
+
+function matchName(drivers, str){
+    return drivers.filter(n => n.name.toLowerCase() === str.toLowerCase());
+}
